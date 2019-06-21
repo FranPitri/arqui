57d7f1d4601d61307d9ba47d62b8be02af03393a
Pitri <franpitri@gmail.com>
2019-06-21 00:25:04 -0300

    update patterns to use delayMillis

--- newton_alg.h
+++ newton_alg.h
@@ -10,7 +10,7 @@ void newton_pattern() {
             unsigned char base = (i < 3) ? 0x18 : 0x30; 
             unsigned char mark = (i < 3) ? 0x80 : 0x8; 
             output(base | (mark >> i % 3));
-            delay(global_speed);
+            delayMillis(global_speed);
         }
 
         for (int i = 0; i<=5; i++) {
@@ -19,7 +19,7 @@ void newton_pattern() {
             unsigned char base = (i < 3) ? 0x30 : 0x18; 
             unsigned char mark = (i < 3) ? 0x1 : 0x20; 
             output(base | (mark << i % 3));
-            delay(global_speed);
+            delayMillis(global_speed);
         }
 
     }
